2792ec8a312154027b08281efca0dfa599e2b37d
Hani <hanialsibai@gmail.com>
2018-10-19 14:55:49 +0100

    First q1 iteration

--- q1.py
+++ q1.py
@@ -5,5 +5,12 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
   # modify and then return the variable below
-  answer = 0
+  anwer = 0
+  for i in range(len(portfolios)):
+      for j in range(len(portfolios)):
+        if i == j:
+            continue
+        else:
+            if portfolios[i]^portfolios[j] > answer:
+                answer = portfolios[i]^portfolios[j]
   return answer
